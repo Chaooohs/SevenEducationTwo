5f0e9fb27374023e2074363cc5011fb7e8f3c8cc
Chaooohs <01nikka01@gmail.com>
2023-06-22 22:35:19 +0300

    asdf

--- html/homework_8/js/style.js
+++ html/homework_8/js/style.js
@@ -1,20 +1,12 @@
 //=================================================================================
 
 const body = document.querySelector("body");
-const nav = document.querySelector(".header__nav");
+const nav = document.querySelector(".header__mobile");
 const burger = document.querySelector(".burger");
-const burgerImg = document.querySelector(".burger__image");
 
 burger.addEventListener("click", function () {
-
   body.classList.toggle("show");
-  const open = nav.classList.toggle("show");
-
-  if (open) {
-    burgerImg.setAttribute("src", "./img/logo/burger-close.svg");
-  } 
-  else {
-    burgerImg.setAttribute("src", "./img/logo/burger-open.svg");
-  }
+  burger.classList.toggle('show')
+  nav.classList.toggle("show");
 });
 
