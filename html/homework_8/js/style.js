10cec7bc6b3fadb41f244ba1760b5235e99da6fe
Chaooohs <01nikka01@gmail.com>
2023-06-24 18:24:40 +0300

    adf

--- html/homework_8/js/style.js
+++ html/homework_8/js/style.js
@@ -6,7 +6,24 @@ const burger = document.querySelector(".burger");
 
 burger.addEventListener("click", function () {
   body.classList.toggle("show");
-  burger.classList.toggle('show')
+  burger.classList.toggle("show");
   nav.classList.toggle("show");
 });
+
+//================================================================================
+
+const link = document.getElementById('el1',)
+
+const mQuery = window.matchMedia("(max-width: 450px)");
+
+function mobileScreen(e) {
+  if (e.matches) {
+    console.log("Media Query Matched!");
+    link.classList.add('works__disabled')
+    
+  }
+}
+
+mQuery.addListener(mobileScreen);
+
 
